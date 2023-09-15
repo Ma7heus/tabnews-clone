e200f0fba5deb0a089720435ad78a5f9b060b3a9
Matheus Henrique S Palacios Pietro Biasi <86833526+Ma7heus@users.noreply.github.com>
2023-09-15 16:08:22 +0000

    Adicionando nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-  return <h1>Eu amo a Laís e estou com muita saudade🤎❤🤎🤎❤</h1>;
+  return  (
+    <html>
+      <head>
+        <title>Minha Carequinha</title>
+      </head>
+      <body>
+        <h1>Eu amo a Laís e estou com muita saudade🤎❤🤎🤎❤</h1>
+        <h2>A Laís é muito fofa</h2>
+        <h2>TE AMO MIL MILHÕES</h2>
+      <a href="https://www.youtube.com/watch?v=orT1lJ7oS5c&ab_channel=TheOneFilmElement">Clique aqui</a>
+
+      </body>
+    </html>
+  );
 }
+
 
 export default Home;
