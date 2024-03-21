099eb9110cafc1a271b1ea675c99eb02dea0a914
Matheus Henrique S Palacios Pietro Biasi <86833526+Ma7heus@users.noreply.github.com>
2024-03-21 01:32:20 +0000

    faz database.js mais robusto para lidar com erros

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,33 @@
-import { Client } from 'pg';
+import { Client, Pool } from 'pg';
+
+const databaseName = process.env.POSTGRES_DB;
+
+async function maxConections() {
+  const result = await query("SHOW max_connections;")
+
+  const max_conection = result.rows[0].max_connections;
+  return max_conection; 
+}
+
+async function databaseVersion() {
+  const result = await query("SHOW server_version;")
+  return result.rows[0].server_version; 
+}
+
+async function openedConnections(){
+  const result = await query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+
+  console.log(result.rows[0].count);
+
+  return result.rows[0].count;
+}
+
 
 async function query(queryObject) {
+  
   const client = new Client({
     host:process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -8,13 +35,22 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  
-  await client.connect();
-  const result = await client.query(queryObject);
-  client.end();
-  return result;
+
+  try{
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(`Erro ao se conectar com banco de dados: ${error}`);
+  } finally {
+    await client.end();
+  }
+ 
 }
 
 export default {
   query: query,
+  maxConections: maxConections,
+  databaseVersion: databaseVersion,
+  openedConnections: openedConnections,
 };
