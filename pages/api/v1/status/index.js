2e00c9d8a0aa88149c6e7433ec457ed445b161d7
Matheus Henrique S Palacios Pietro Biasi <86833526+Ma7heus@users.noreply.github.com>
2024-03-21 01:33:08 +0000

    finaliza endpoint ´/status´

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,22 @@
 import database from 'infra/database.js'
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows)
+  const updatetAt = new Date().toISOString();
+  const databaseVersion = await database.databaseVersion();
+  const databaseMaxConections = await database.maxConections();
+  const databaseOpenedConnections = await database.openedConnections(); 
 
-  res.status(200).send("API STATUS RODANDO")
+  res.status(200).json({
+    updated_at: updatetAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConections),
+        connections: databaseOpenedConnections
+      }
+    }
+  });
+
 }
 
 export default status;
--- tests/integration/api/status/get.test.js
+++ tests/integration/api/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status")
-    expect(response.status).toBe(200);
+  const response = await fetch("http://localhost:3000/api/v1/status")
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parseUpdatedAt).toEqual(responseBody.updated_at)
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+
+  expect(responseBody.dependencies.database.connections).toEqual(1);
+
+  // expect(responseBody.conections).toBeDefined();
+  
 });
